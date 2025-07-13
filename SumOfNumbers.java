b289c0b3ea82813c15d1b9abf756d5b81ea7fa33
Gayathri-B-Nair <gayathribnair2028@cs.sjcetpalai.ac.in>
2025-07-13 21:58:55 +0530

    Add files via upload

---
+++ SumOfNumbers.java
@@ -0,0 +1,9 @@
+package module1;
+
+public class SumOfNumbers {
+	public static void main(String[] args) {
+		int a=10,b=50;
+		System.out.println("Sum of a & b:"+(a+b));
+		}
+
+}
---
+++ TernaryOperatorDemo.java
@@ -0,0 +1,16 @@
+package module1;
+import java.util.Scanner;
+public class TernaryOperatorDemo {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int firstNumber,secondNumber;
+		System.out.println("Enter the first number");
+		firstNumber=sc.nextInt();
+		System.out.println("Enter the second number");
+		secondNumber=sc.nextInt();
+		int bigNumber;
+		bigNumber=firstNumber>secondNumber?firstNumber:secondNumber;
+		System.out.println("I am "+bigNumber+",the big number");
+	}
+
+}
